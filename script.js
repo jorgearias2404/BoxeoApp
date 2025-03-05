080413229338db1cde5b218e74600ec355fc20b1
Jorge Arias <ja549655@gmail.com>
2025-03-05 15:16:17 -0400

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+const Botones = document.querySelectorAll('button');
+const Inputs = document.querySelectorAll('input')
+
+
+const Reinciar = Botones[0];
+const NuevosPeleadores = Botones[1];
+const Resultado = Botones[2];
+
+Reinciar.addEventListener('click',()=>{
+    Inputs.forEach(dato,()=>{
+        data.value
+    });
+})
